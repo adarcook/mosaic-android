d95f5880c95cb7742c1fa3d48079fc5b97c8aca0
adarcook <adar4cook@gmail.com>
2026-07-26 17:15:06 +0300

    Use Health Connect version compatible with compileSdk 35

--- feature/training/build.gradle.kts
+++ feature/training/build.gradle.kts
@@ -27,5 +27,5 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-    implementation("androidx.health.connect:connect-client:1.1.0-rc03")
+    implementation("androidx.health.connect:connect-client:1.1.0-alpha10")
 }
