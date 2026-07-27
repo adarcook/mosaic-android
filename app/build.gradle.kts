6a0d824938e2c5e687548fc677b13e9f143f7599
adarcook <adar4cook@gmail.com>
2026-07-27 12:08:36 +0300

    Raise app minSdk for Samsung Health Data SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "life.mosaic.fit"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
