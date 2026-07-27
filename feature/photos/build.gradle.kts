6003976fd94003701bec5c61154ae5a2eed276b6
adarcook <adar4cook@gmail.com>
2026-07-27 17:01:22 +0300

    Connect Photos feature to core database

--- feature/photos/build.gradle.kts
+++ feature/photos/build.gradle.kts
@@ -21,6 +21,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:database"))
     implementation(platform("androidx.compose:compose-bom:2025.01.01"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
