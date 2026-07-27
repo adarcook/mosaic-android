442d29fb1c6edcdb06f5a6ef4f3aa1680f38b379
adarcook <adar4cook@gmail.com>
2026-07-27 14:23:01 +0300

    Add Android core dependency for photo permissions

--- feature/photos/build.gradle.kts
+++ feature/photos/build.gradle.kts
@@ -25,5 +25,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.core:core-ktx:1.15.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
